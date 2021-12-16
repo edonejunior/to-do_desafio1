d3e515eb109ae3d126c7d9b8e070fbb65c40472b
Edvaldo Junior <edonejunior@hotmail.com>
2021-12-16 18:02:30 -0300

    To - Solução Task Done (dificil)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,39 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const task = {
+      id : new Date().getTime(),
+      title:newTaskTitle,
+      done:false
+    }
+
+    //Pelo set eu recupero todos os taks anteriores e incluo a nova.
+    setTasks(oldTasks => [...oldTasks, task])
+    
   }
 
   function handleToggleTaskDone(id: number) {
+    //Quando não é tipo primitivo ele pega a referência da memória e por isso ele trás os tasks completo.
+
+    const updatedTasks = tasks.map(task => ({...task}))
+    const updatedTask = updatedTasks.find(tarefa => tarefa.id === id)
+    
+    if(!updatedTask)
+      return;
+
+    //Diferente do que já tem
+    updatedTask.done = !updatedTask.done;
+
+    setTasks(updatedTasks);
+
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks(oldTasks => oldTasks.filter(
+      task => task.id !== id
+    ))
   }
 
   return (
